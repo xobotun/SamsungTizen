cdfe626a085c8c3389e7c16ecbf746da9c5304b0
xobotun <xobotun@inbox.ru>
2016-07-19 11:03:49 +0300

    Now random text color on third page.

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -59,3 +59,16 @@ function showScreenData() {
 	
 	div.innerHTML = " " + x + " × " + y;
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function colorifyThirdText() {
+	$("#third__text").css("color", getRandomColor());
+}
